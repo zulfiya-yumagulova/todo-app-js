bb7aa2df0f53df2e5709d6332f68fddb15f7ba75
Zulfiya Yumagulova <yumagulovazu@gmail.com>
2022-09-05 13:11:16 +0100

    createtd a function to add tasks

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
+// const form = document.querySelector("#new-task");
+// const input = document.querySelector(".input-task");
+// const addBtn = document.querySelector(".btn-add");
+// const tasksList = document.querySelector("#tasks");
 
+// Creating load page
+window.addEventListener("load", () => {
+  const form = document.querySelector("#new-task");
+  const input = document.querySelector(".input-task");
+  const addBtn = document.querySelector(".btn-add");
+  const tasksList = document.querySelector("#tasks");
+  console.log(addBtn);
+  // Stop refreshing the page
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const task = input.value;
+
+    if (!task) {
+      alert("Please fill out the task");
+      return;
+    }
+    const taskElement = document.createElement("div");
+    taskElement.classList.add("task");
+
+    const taskContentEl = document.createElement("div");
+    taskContentEl.classList.add("content");
+
+    taskElement.appendChild(taskContentEl);
+
+    const inputNewTask = document.createElement("input");
+    inputNewTask.classList.add("text");
+    inputNewTask.type = "text";
+    inputNewTask.value = task;
+    inputNewTask.setAttribute("readonly", "readonly");
+    taskContentEl.appendChild(inputNewTask);
+
+    tasksList.appendChild(taskElement);
+  });
+});
